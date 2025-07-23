2636d8a38adfe808d962ddeb519e7b731d4e5728
PestyLint <me@example.com>
2025-07-23 17:08:01 -0400

    Remove configFile.source setting from nushell configuration in nu.nix

--- modules/home-manager/nu.nix
+++ modules/home-manager/nu.nix
@@ -1,7 +1,5 @@
 {inputs, config, pkgs, ...}: {
     programs = {
-    nushell = { enable = true;
-      configFile.source = /Users/justin/.config/nushell/config.nu --impure;
-        };
+    nushell = { enable = true};
     };
 }
