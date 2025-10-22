7fcf1e83b33bbdce1d09de87758660f1065d6121
PestyLint <me@example.com>
2025-10-22 16:21:12 -0400

    Update flake.lock and add sops configuration for secrets management

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,22 +1,22 @@
 {inputs, config, pkgs, ...}: {
   programs.git = { #Nix was smashing git config. Added 03.25
     enable = true;
-    userName = "PestyLint";
-    userEmail = "me@example.com";
+    settings = {
+      user.name = "PestyLint";
+      user.email = "me@example.com";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      fetch.prune = true;
+      fetch.writeCommitGraph = true;
+      branch.sort = "-committerdate";
+      commit.gpgSign = true;
+      tag.gpgSign = true;
+      gpg.format = "ssh";
+      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILi+OcRdb7L6hiLF0LO0UqktVP3iXSqrESQQ8YD8MoHJ";
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    };
     includes = [
       { path = "~/.gitconfig.local"; }
     ];
-    extraConfig = {
-        push.autoSetupRemote = true;
-        pull.rebase = true;
-        fetch.prune = true;
-        fetch.writeCommitGraph = true;
-        branch.sort = "-committerdate";
-        commit.gpgSign = true;
-        tag.gpgSign = true;
-        gpg.format = "ssh";
-        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILi+OcRdb7L6hiLF0LO0UqktVP3iXSqrESQQ8YD8MoHJ";
-        gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-    };
   };
 }
